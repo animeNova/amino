16af5844b86b73502b8f4f03812ff23ac3f1ec5a
Otakukorean <bboomstation1@gmail.com>
2025-05-20 01:51:37 +0300

    fix auth error

--- lib/auth/client.ts
+++ lib/auth/client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 import { adminClient, magicLinkClient  } from "better-auth/client/plugins";
 export const {signIn,signOut,getSession,useSession,magicLink,admin,updateUser} = createAuthClient({
-    baseURL: "http://localhost:3000", // the base url of your auth server,
+    baseURL: process.env.BETTER_AUTH_URL, // the base url of your auth server,
     plugins : [
         magicLinkClient(),
         adminClient(),
